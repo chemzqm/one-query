82f92eb32be08d49ee0af83096e29471d7dbac44
Qiming Zhao <chemzqm@gmail.com>
2013-07-26 15:39:37 +0800

    first commit

---
+++ Readme.md
@@ -0,0 +1,18 @@
+
+# query-cache
+
+  Make simple ajax get request with cache support
+
+## Installation
+
+  Install with [component(1)](http://component.io):
+
+    $ component install chemzqm/query-cache
+
+## API
+
+
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,67 @@
+/*jshint -W116,-W069,-W098,-W003*/
+var request = require('superagent');
+
+var encode = encodeURIComponent;
+
+module.exports = function(url, params, callback) {
+  if (arguments.length < 3) {
+    callback = params;
+    params = null;
+  }
+
+  var pairs = [];
+  for (var key in params) {
+    pairs.push(encode(key)
+      + '=' + encode(params[key]));
+  }
+  if (params) {
+    url = url + '?' + pairs.join('&');
+  }
+  queues.add(url, callback);
+}
+
+var queues = (function() {
+  var map = {};
+  return {
+    add: function(url, cb) {
+      var queue = map[url];
+      if (queue)
+        return queue.push(cb);
+      queue= new Queue(url);
+      map[url] = queue;
+      queue.push(cb);
+      queue.start();
+    },
+    get: function(url) {
+      return map[url];
+    }
+  }
+})();
+
+function Queue (url) {
+  this.url = url;
+  this.callbacks = [];
+  this.status = '';
+  this.err = null;
+  this.res = null;
+}
+
+Queue.prototype.push = function(cb) {
+  if (this.status === 'done') {
+    return cb(this.err, this.res);
+  }
+  this.callbacks.push(cb);
+}
+
+Queue.prototype.start = function() {
+  this.status = 'pendding';
+  request.get(this.url, function(err, res) {
+    this.err = err;
+    this.res = res;
+    this.status = 'done';
+    this.callbacks.forEach(function(cb) {
+      cb(err, res);
+    });
+  this.callbacks = [];
+  }.bind(this));
+}
